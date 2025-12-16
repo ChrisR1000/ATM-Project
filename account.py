c59e15ef6e78d640debd19f711421f2303adb2d6
Christopher Rojo <christopherrojo70@gmail.com>
2025-12-15 19:55:47 -0800

    Update on account.py file

--- account.py
+++ account.py
@@ -1,14 +1,31 @@
-
-
-
 #Account Class for atm project
 class Account:
-    def __init__(self, client_name, account_number, pin, balance=0.0):
-        self.client_name = client_name
+    def __init__(self, account_name, account_number, pin, balance=0.0):
+        self.account_name = account_name
         self.account_number = account_number
         self.pin = pin
         self.balance = balance
     
-    def create_account(self):
-        new_account = self.account_number
+    @classmethod
+    def create_account(self, name, pin):
+       name = input("Enter the name for new bank account: ")
+       self.account_name = name
+       pin = input("Enter pin for new account: ")
+       self.pin = pin 
+
+    def deposit(self, amount):
+        if amount >= 0:
+            self.balance += amount
+            print(f"${amount} has been deposited successfully. Your new balance is {self.balance}.")
+        else:
+            print("Invalid deposit amount.")
+
+    def withdraw(self, amount): 
+        if amount >= 0:
+            self.balance -= amount
+            print(f"{amount} has been withdrawn from your account. Your balance is now{self.balance}.")
+        else:
+            print("Invalid amount.")
+    
+    #Update to ATM Project
 
